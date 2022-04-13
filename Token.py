324064adbd41849bd17cb12177f293b859fb06fa
Orazjan <45052100+Orazjan@users.noreply.github.com>
2022-04-13 19:06:18 +0300

    Update Token.py

--- Token.py
+++ Token.py
@@ -1,7 +1,7 @@
 from tokenize import Token
 from aiogram import Bot, bot, Dispatcher
 
-Api_Token = "5143523500:AAEdLVpkBxgS9N-TQFVI2-GD6URZ6VCyts0"
+Api_Token = "Your TOKEN from BotFather"
 
 bot = Bot(token=Api_Token)
 dp = Dispatcher(bot)
