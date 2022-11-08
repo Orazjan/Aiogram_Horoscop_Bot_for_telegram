6c0fa230fab8f7dafdb71ddd6059208596bfe659
Orazjan <orazjanov11@gmail.com>
2022-11-08 15:46:07 +0300

    corrected token

--- Token.py
+++ Token.py
@@ -1,5 +1,7 @@
 from aiogram import Bot, bot, Dispatcher
 
+#Api_Token you have enter your own token from bot father
+
 Api_Token = "5143523500:AAEdLVpkBxgS9N-TQFVI2-GD6URZ6VCyts0"
 
 bot = Bot(token=Api_Token)
